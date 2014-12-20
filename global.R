13d95634b442993b0c423bff6ebcf4bda72b7dfb
mdlincoln <matthew.d.lincoln@gmail.com>
2014-12-20 15:03:08 -0500

    better comments for global.R

--- global.R
+++ global.R
@@ -1,18 +1,28 @@
 library(igraph)
 library(dplyr)
 
+# Read in CSV files with edge and node attributes
 original_edgelist <- read.csv("goltzius_graph.csv", stringsAsFactors = FALSE)
 original_nodelist <- read.csv("goltzius_nodes.csv", stringsAsFactors = FALSE)
+
+# Create iGraph object
 graph <- graph.data.frame(original_edgelist, directed = TRUE, vertices = original_nodelist)
 
+# Calculate various network properties, adding them as attributes to each
+# node/vertex
 V(graph)$comm <- membership(optimal.community(graph))
 V(graph)$degree <- degree(graph)
 V(graph)$closeness <- centralization.closeness(graph)$res
 V(graph)$betweenness <- centralization.betweenness(graph)$res
 V(graph)$eigen <- centralization.evcent(graph)$vector
 
+# Re-generate dataframes for both nodes and edges, now containing
+# calculated network attributes
 node_list <- get.data.frame(graph, what = "vertices")
 
+# Determine a community for each edge. If two nodes belong to the
+# same community, label the edge with that community. If not,
+# the edge community value is 'NA'
 edge_list <- get.data.frame(graph, what = "edges") %>%
   inner_join(node_list %>% select(name, comm), by = c("from" = "name")) %>%
   inner_join(node_list %>% select(name, comm), by = c("to" = "name")) %>%
