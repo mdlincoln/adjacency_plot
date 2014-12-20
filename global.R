ac9593ea2b602eceeb8afe6dfaf2a9ff652455d8
mdlincoln <matthew.d.lincoln@gmail.com>
2014-12-20 15:00:31 -0500

    Don't order the communities - too complex for this example

--- global.R
+++ global.R
@@ -12,8 +12,6 @@ V(graph)$betweenness <- centralization.betweenness(graph)$res
 V(graph)$eigen <- centralization.evcent(graph)$vector
 
 node_list <- get.data.frame(graph, what = "vertices")
-comm_order <- (node_list %>% count(comm) %>% arrange(-n))$comm
-node_list <- node_list %>% mutate(comm = factor(comm, comm_order))
 
 edge_list <- get.data.frame(graph, what = "edges") %>%
   inner_join(node_list %>% select(name, comm), by = c("from" = "name")) %>%
