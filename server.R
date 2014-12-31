5b11bbfddf4c79cf48b75063c0f39d59f10b2fab
mdlincoln <matthew.d.lincoln@gmail.com>
2014-12-31 18:54:02 -0500

    paste group members into one line

--- server.R
+++ server.R
@@ -108,10 +108,10 @@ shinyServer(function(input, output, session) {
     member_html <- list()
     for(i in comms) {
       group_membs <- members$name[members$comm == i]
-      member_html[[i]] <- list(h3("Group", i))
-      for(a in group_membs) {
-        member_html[[i]][[a]] <- list(p(a))
-      }
+      member_html[[i]] <- list(h3("Group", i), p(paste(group_membs, collapse = ", ")))
+#       for(a in group_membs) {
+#         member_html[[i]][[a]] <- list(a)
+#       }
     }
     return(member_html)
   })
