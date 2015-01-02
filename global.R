5c7bc00581361a3d6b46c9e79793fc28d4a9e1f4
mdlincoln <matthew.d.lincoln@gmail.com>
2015-01-02 11:34:16 -0500

    Encapsulate network analysis within a function, and save results to RData. This way the app can load quickly, but users can also see what is going on under the hood

--- global.R
+++ global.R
@@ -1,51 +1,56 @@
 library(igraph)
 library(dplyr)
 
-# Read in CSV files with edge and node attributes
-goltzius_edgelist <- read.csv("goltzius_graph.csv", stringsAsFactors = FALSE)
-goltzius_nodelist <- read.csv("goltzius_nodes.csv", stringsAsFactors = FALSE)
+generate_graph_tables <- function(edge_table, node_table = NULL, directed = TRUE) {
+  # Create iGraph object
+  print("Creating Igraph object...")
+  graph <- graph.data.frame(edge_table, directed = directed, vertices = node_table)
 
-# Create iGraph object
-goltzius_graph <- graph.data.frame(goltzius_edgelist, directed = TRUE, vertices = goltzius_nodelist)
+  # Calculate various network properties, adding them as attributes to each
+  # node/vertex
+  print("Walktrap communities...")
+  V(graph)$walktrap_comm <- membership(walktrap.community(graph))
+  print("Spinglass commmunities")
+  V(graph)$spinglass_comm <- membership(spinglass.community(graph))
+  print("Edge betweenness communities...")
+  V(graph)$edge_comm <- membership(edge.betweenness.community(graph))
+  print("Other node statistics...")
+  V(graph)$degree <- degree(graph)
+  V(graph)$closeness <- centralization.closeness(graph)$res
+  V(graph)$betweenness <- centralization.betweenness(graph)$res
+  V(graph)$eigen <- centralization.evcent(graph)$vector
 
-# Calculate various network properties, adding them as attributes to each
-# node/vertex
-V(goltzius_graph)$optimal_comm <- membership(optimal.community(goltzius_graph))
-V(goltzius_graph)$walktrap_comm <- membership(walktrap.community(goltzius_graph))
-V(goltzius_graph)$spinglass_comm <- membership(spinglass.community(goltzius_graph))
-V(goltzius_graph)$edge_comm <- membership(edge.betweenness.community(goltzius_graph))
-V(goltzius_graph)$degree <- degree(goltzius_graph)
-V(goltzius_graph)$closeness <- centralization.closeness(goltzius_graph)$res
-V(goltzius_graph)$betweenness <- centralization.betweenness(goltzius_graph)$res
-V(goltzius_graph)$eigen <- centralization.evcent(goltzius_graph)$vector
+  # Re-generate dataframes for both nodes and edges, now containing
+  # calculated network attributes
+  node_list <- get.data.frame(graph, what = "vertices")
+  edge_list <- get.data.frame(graph, what = "edges")
 
-# Re-generate dataframes for both nodes and edges, now containing
-# calculated network attributes
-goltzius_node_list <- get.data.frame(goltzius_graph, what = "vertices")
-goltzius_edge_list <- get.data.frame(goltzius_graph, what = "edges")
+  if(directed) {
+    return(list(nodes = node_list, edges = edge_list))
+  } else {
+    edge_list <- rbind(edge_list, edge_list %>% rename(from = to, to = from))
+    return(list(nodes = node_list, edges = edge_list))
+  }
+}
 
+if(file.exists("data/goltzius.RData")) {
+  load("data/goltzius.RData")
+} else {
+  goltzius_edgelist <- read.csv("data/csv/goltzius_graph.csv", stringsAsFactors = FALSE)
+  goltzius_nodelist <- read.csv("data/csv/goltzius_nodes.csv", stringsAsFactors = FALSE)
+  goltzius_tables <- generate_graph_tables(goltzius_edgelist, goltzius_nodelist, directed = TRUE)
+  goltzius_node_list <- goltzius_tables$nodes
+  goltzius_edge_list <- goltzius_tables$edges
+  save(goltzius_edge_list, goltzius_node_list, file = "data/goltzius.RData")
+}
 
+if(file.exists("data/les_mis.RData")) {
+  load("data/les_mis.RData")
+} else {
+  lm_edgelist <- read.csv("data/csv/les_mis_graph.csv", stringsAsFactors = FALSE)
+  lm_tables <- generate_graph_tables(lm_edgelist, directed = FALSE)
+  lm_node_list <- lm_tables$nodes
+  lm_edge_list <- lm_tables$edges
+  save(lm_node_list, lm_edge_list, file = "data/les_mis.RData")
+}
 
-
-lm_edgelist <- read.csv("les_mis_graph.csv", stringsAsFactors = FALSE)
-
-# Create iGraph object
-lm_graph <- graph.data.frame(lm_edgelist, directed = FALSE)
-
-# Calculate various network properties, adding them as attributes to each
-# node/vertex
-V(lm_graph)$optimal_comm <- membership(optimal.community(lm_graph))
-V(lm_graph)$walktrap_comm <- membership(walktrap.community(lm_graph))
-V(lm_graph)$spinglass_comm <- membership(spinglass.community(lm_graph))
-V(lm_graph)$edge_comm <- membership(edge.betweenness.community(lm_graph))
-V(lm_graph)$degree <- degree(lm_graph)
-V(lm_graph)$closeness <- centralization.closeness(lm_graph)$res
-V(lm_graph)$betweenness <- centralization.betweenness(lm_graph)$res
-V(lm_graph)$eigen <- centralization.evcent(lm_graph)$vector
-
-# Re-generate dataframes for both nodes and edges, now containing
-# calculated network attributes
-lm_node_list <- get.data.frame(lm_graph, what = "vertices")
-lm_edge_list <- get.data.frame(lm_graph, what = "edges")
-# "mirror" the undirected edge list
-lm_edge_list <- rbind(lm_edge_list, lm_edge_list %>% select(from = to, to = from, weight))
